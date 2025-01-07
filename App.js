b192d1ed6ca0e7bb1cc47e8f803f91689ebac9af
Quanbeothoima123 <phimanhh85@gmail.com>
2025-01-07 19:07:17 +0700

    Bai2.1 lan 1

--- App.js
+++ App.js
@@ -1,40 +1,21 @@
 import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView, Text, View } from "react-native";
 import Box from "./component/box";
+import SquareBox from "./component/squareBox";
 
 export default function App() {
   return (
     <SafeAreaView style={{ flex: 1, padding: 20, paddingTop: 40 }}>
-      {/* Box 1 */}
-      <Box
-        title="Box 1"
-        borderColor="blue"
-        borderWidth={3}
-        myCustomProp="Custom content for Box 1"
-        style={{ backgroundColor: "#E8F6FF" }}
-      >
-        <Text style={{ fontSize: 18 }}>This is the first Box</Text>
-      </Box>
-
-      {/* Box 2 */}
-      <Box
-        title="Box 2"
-        borderColor="green"
-        borderWidth={5}
-        myCustomProp="Custom content for Box 2"
-        style={{ backgroundColor: "#F0FFF4" }}
-      >
-        <Text style={{ fontSize: 18, color: "green" }}>
-          This is the second Box
-        </Text>
-      </Box>
-
-      {/* Box 3 without title and myCustomProp */}
-      <Box borderColor="red" borderWidth={1}>
-        <Text style={{ fontSize: 18 }}>
-          This is the third Box without a title
-        </Text>
-      </Box>
+      <View>
+        <SquareBox color={"yellow"} title={"Hello World"}>
+          {""}
+        </SquareBox>
+      </View>
+      <View>
+        <SquareBox color={"green"} title={"Xin chào mọi người"}>
+          {""}
+        </SquareBox>
+      </View>
     </SafeAreaView>
   );
 }
---
+++ component/buttonCustom.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+const ButtonCustom = () => {
+  return <button>Click me</button>;
+};
+
+export default ButtonCustom;
