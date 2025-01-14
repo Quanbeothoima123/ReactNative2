820d53e3e7d5e1aa8ba745dca66f93b69fb81f7e
Quanbeothoima123 <phimanhh85@gmail.com>
2025-01-14 14:09:15 +0700

    version2

--- App.js
+++ App.js
@@ -1,57 +1,106 @@
 import React from "react";
 import {
-  FlatList,
+  SectionList,
   Text,
   View,
   StyleSheet,
   TouchableOpacity,
   Alert,
   SafeAreaView,
+  FlatList,
+  ScrollView,
 } from "react-native";
 
 const App = () => {
-  // Danh sách sản phẩm
   const products = [
     { id: "1", name: "Pixel 4xl", price: "100$" },
     { id: "2", name: "Iphone 15promax", price: "1500$" },
     { id: "3", name: "Iqoo 13", price: "500$" },
   ];
 
-  // Hàm xử lý khi bấm vào sản phẩm
+  const groupedProducts = [
+    {
+      title: "iPhone",
+      data: ["iPhone 15 Pro", "iPhone 14", "iPhone SE (2023)"],
+    },
+    {
+      title: "Google Pixel",
+      data: ["Google Pixel 8 Pro", "Google Pixel 7a", "Google Pixel Fold"],
+    },
+    { title: "Vivo", data: ["Vivo X90 Pro", "Vivo Y78", "Vivo V29 5G"] },
+    {
+      title: "Các sản phẩm mới nhất",
+      data: ["Samsung Galaxy S23 Ultra", "OnePlus 12", "Xiaomi 14 Pro"],
+    },
+  ];
+
   const handleProductPress = (productName) => {
     Alert.alert("Thông báo", `Tên sản phẩm: ${productName}`);
   };
 
-  // Hàm hiển thị từng item
+  const renderItemSection = ({ item }) => (
+    <TouchableOpacity onPress={() => handleProductPress(item)}>
+      <View style={styles.itemContainer}>
+        <Text style={styles.productName}>{item}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+
   const renderItem = ({ item }) => (
     <TouchableOpacity onPress={() => handleProductPress(item.name)}>
       <View style={styles.itemContainer}>
         <Text style={styles.productName}>{item.name}</Text>
-        <Text style={styles.productPrice}>${item.price}</Text>
+        <Text style={styles.productPrice}>{item.price}</Text>
       </View>
     </TouchableOpacity>
   );
 
   return (
     <SafeAreaView style={styles.container}>
-      <View>
-        <FlatList
-          data={products}
-          keyExtractor={(item) => item.id}
-          renderItem={renderItem}
-        />
-      </View>
+      <ScrollView>
+        {/* SectionList */}
+        <View style={{ marginBottom: 20 }}>
+          <Text style={styles.listTitle}>Danh sách nhóm sản phẩm</Text>
+          <SectionList
+            sections={groupedProducts}
+            keyExtractor={(item, index) => item + index}
+            renderItem={renderItemSection}
+            renderSectionHeader={({ section }) => (
+              <View style={styles.headerContainer}>
+                <Text style={styles.sectionTitle}>{section.title}</Text>
+              </View>
+            )}
+            nestedScrollEnabled
+          />
+        </View>
+
+        {/* FlatList */}
+        <View>
+          <Text style={styles.listTitle}>Danh sách sản phẩm</Text>
+          <FlatList
+            data={products}
+            keyExtractor={(item) => item.id}
+            renderItem={renderItem}
+            nestedScrollEnabled
+          />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
 
-// Styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#f8f8f8",
     padding: 16,
     paddingTop: 40,
+  },
+  listTitle: {
+    fontSize: 22,
+    fontWeight: "bold",
+    marginBottom: 10,
+    color: "#333",
   },
   itemContainer: {
     flexDirection: "row",
@@ -72,7 +121,17 @@ const styles = StyleSheet.create({
   },
   productPrice: {
     fontSize: 14,
-    color: "#555",
+    color: "#888",
+  },
+  headerContainer: {
+    padding: 10,
+    backgroundColor: "yellow",
+    borderRadius: 20,
+    marginBottom: 8,
+  },
+  sectionTitle: {
+    fontSize: 20,
+    color: "blue",
   },
 });
 
